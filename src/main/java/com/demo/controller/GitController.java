3dbd58c4b3e669a26b31abd8f274cfd3ad7e837d
liyongchang <@1234567890>
2019-06-05 21:47:26 +0800

    update

--- src/main/java/com/demo/controller/GitController.java
+++ src/main/java/com/demo/controller/GitController.java
@@ -2,10 +2,16 @@ package com.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("gittest")
 public class GitController {
 
+    @ResponseBody
+    @RequestMapping("hello")
+    public String hello(){
+        return "hello";
+    }
 
 }
