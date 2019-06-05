63538855722422a7b1f687afe8d68063e3a88418
liyongchang <@1234567890>
2019-06-05 21:37:13 +0800

    Initial commit

---
+++ src/main/java/com/demo/controller/GitController.java
@@ -0,0 +1,11 @@
+package com.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("gittest")
+public class GitController {
+
+
+}
